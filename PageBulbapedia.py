aa82de7b50f39f180e41715e07c9565515ff0246
dr-spangle <dr-spangle@dr-spangle.com>
2015-05-01 10:28:25 +0100

    Added getAnimeDexEntries to PageBulbasaur object

--- PageBulbapedia.py
+++ PageBulbapedia.py
@@ -1,4 +1,5 @@
 from Commons import Commons
+import re
 
 class PageBulbapedia(object):
     '''
@@ -19,3 +20,20 @@ class PageBulbapedia(object):
         except:
             self.mCode = Commons.downloadPage(link)
             open("cache/bulbapedia/"+format(dexNum,'03')+".html","wb").write(self.mCode)
+    
+    def getAnimeDexEntries(self):
+        animeDexRegex = re.compile('<h3><span[^>]*>[^<]*dex entries</span></h3>(.*?)<h3>',re.DOTALL|re.IGNORECASE)
+        animeDexEntryRegex = re.compile('<td> *<a[^>]*>([A-Z]+[0-9]+)</a>[^<]*</td>[^<]*<td> *<strong class="selflink">[^<]*</strong>[^<]*</td>[^<]*<td>[^<]*</td>[^<]*<td>(.*?)</td>',re.DOTALL|re.IGNORECASE)
+        tagRegex = Commons.tagRegex
+        animeDexSearch = re.search(animeDexRegex,self.mCode)
+        if(animeDexSearch is None):
+            print("no anime dex section")
+            continue
+        animeDexCode = animeDexSearch.group(1)
+        outputList = []
+        for animeDexEntrySearch in re.finditer(animeDexEntryRegex,animeDexCode):
+            animeDexDict = {}
+            animeDexDict['episode'] = animeDexEntrySearch.group(1)
+            animeDexDict['entry'] = tagRegex.sub('',animeDexEntrySearch.group(2)).strip()
+            outputList.append(animeDexDict)
+        return outputList
