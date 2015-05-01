5122496c3cc0e133f329159d75b81738350b8bf2
dr-spangle <dr-spangle@dr-spangle.com>
2015-05-01 12:34:12 +0100

    Added a Commons.loadPage() method, to better utilise cache

--- Commons.py
+++ Commons.py
@@ -1,5 +1,6 @@
 import urllib.request
 import re
+import os
 
 class Commons:
     '''
@@ -10,8 +11,25 @@ class Commons:
     @staticmethod
     def downloadPage(url):
         #time.sleep(3)
+        print("Downloading "+url)
         pageRequest = urllib.request.Request(url)
         pageRequest.add_header('User-Agent','Mozilla/5.0 (X11; Linux i686; rv:23.0) Gecko/20100101 Firefox/23.0')
         pageOpener = urllib.request.build_opener()
         code = pageOpener.open(pageRequest).read()
         return code
+    
+    @staticmethod
+    def loadPage(cacheFileName,url):
+        try:
+            cacheFile = open(cacheFileName,"rb")
+            code = cacheFile.read()
+        except:
+            code = Commons.downloadPage(url)
+            for dirName in ["/".join(cacheFileName.split("/")[:x]) for x in range(1,len(cacheFileName.split("/")))]:
+                try:
+                    os.mkdir(dirName)
+                except:
+                    pass
+            open(cacheFileName,"wb").write(code)
+        return code
+        
