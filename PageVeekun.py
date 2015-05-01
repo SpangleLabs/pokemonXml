5aace698ea74220f2a76050e571ac9c5d58eacbf
dr-spangle <dr-spangle@dr-spangle.com>
2015-05-01 11:59:58 +0100

    Framework object for PageVeekun

---
+++ PageVeekun.py
@@ -0,0 +1,36 @@
+from Commons import Commons
+from Page import Page
+from PageBulbapediaEdit import PageBulbapediaEdit
+
+
+class PageGeneric:
+    '''
+    generic page object, template for others.
+    '''
+    mDexNum = None  #(National) Pokedex number of pokemon
+    mLink = None    #Page link
+    mCode = None    #Code of page
+
+
+    def __init__(self,dexNum,link=None):
+        '''
+        Constructor
+        '''
+        self.mDexNum = dexNum
+        if(link is None):
+            link = self.findLink(dexNum)
+        self.mLink = link
+        try:
+            cacheFile = open("cache/veekun/"+format(dexNum,'03')+".html","rb")
+            self.mCode = cacheFile.read()
+        except:
+            self.mCode = Commons.downloadPage(link)
+            open("cache/veekun/"+format(dexNum,'03')+".html","wb").write(self.mCode)
+
+    @staticmethod
+    def findLink(dexNum):
+        'Finds the page link for this dex number'
+        bulbaEditObject = PageBulbapediaEdit(dexNum)
+        pokemonName = bulbaEditObject.getTemplateValue("pokefordex")
+        linkVeekun = "http://veekun.com/dex/pokemon/"+pokemonName
+        return linkVeekun
