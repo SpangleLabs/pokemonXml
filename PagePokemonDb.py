e0c41dea3a807ff0529bb2cf637ce5d69bbfa184
dr-spangle <dr-spangle@dr-spangle.com>
2015-05-01 10:57:37 +0100

    Starting work on PagePokemonDb object

---
+++ PagePokemonDb.py
@@ -0,0 +1,31 @@
+from Commons import Commons
+from Page import Page
+
+class PagePokemonDb(Page):
+    '''
+    pokemonDB page object. For retrieving information from a pokemonDB page
+    '''
+    mDexNum = None  #(National) Pokedex number of pokemon
+    mLink = None    #Page link
+    mCode = None    #Code of page
+
+
+    def __init__(self,dexNum,link=None):
+        '''
+        Constructor
+        '''
+        self.mDexNum = dexNum
+        if(link is None):
+            link = self.findLink(dexNum)
+        self.mLink = link
+        try:
+            cacheFile = open("cache/pokemondb/"+format(dexNum,'03')+".html","rb")
+            self.mCode = cacheFile.read()
+        except:
+            self.mCode = Commons.downloadPage(link)
+            open("cache/pokemondb/"+format(dexNum,'03')+".html","wb").write(self.mCode)
+
+    @staticmethod
+    def findLink(dexNum):
+        'Finds the pokemondb link for this dex number'
+        return "http://pokemondb.net/pokedex/"+str(dexNum)
